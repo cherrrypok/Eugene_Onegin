feeb2d964dcfc82849bf6d87ac287b6290d9324d
cherrrypok <tashchershit@gmail.com>
2021-09-30 03:36:25 +0300

    +File

--- Eugene Onegin/Eugene_Onegin.c
+++ Eugene Onegin/Eugene_Onegin.c
@@ -8,16 +8,17 @@ struct String {
     int len;
 };
 
-int Read_strings(struct String* lineptr, int maxsize, int* lines);
+int Read_strings(struct String* lineptr, int maxsize, int* lines, FILE *File);
 int Write_strings(struct String* lineptr, int* lines);
-int getline(char* s, int maxsize);
+int getline(char* s, int maxsize, FILE *File);
 int Unit_tests(struct String* lineptr);
 
 int main() {
+    FILE *File = fopen("text_input.txt", "r");
     int lines = 0;
     struct String lineptr[MAXLINES];
 
-    Read_strings(lineptr, MAXLINES, &lines);
+    Read_strings(lineptr, MAXLINES, &lines, File);
     Write_strings(lineptr, &lines);
     //printf("%d\n", lines);
     //Unit_tests(lineptr);
@@ -30,10 +31,10 @@ int Unit_tests(struct String* lineptr) {
 
 }
 
-int getline(char* s, int maxsize) {
+int getline(char* s, int maxsize, FILE *File) {
     char c;
     int i = 0;
-    while (maxsize-- && (c = getchar()) != EOF && c != '\n')
+    while (maxsize-- && (c = getc(File)) != EOF && c != '\n')
         s[i++] = c;
     if (c == '\n')
         s[i++] = '\n';
@@ -41,17 +42,17 @@ int getline(char* s, int maxsize) {
     return --i;
 }
 
-int Read_strings(struct String* lineptr, int maxsize, int* lineptr_index) {
+int Read_strings(struct String* lineptr, int maxsize, int* lineptr_index, FILE *File) {
     int len = 0;
     char* line = (char*)calloc(MAXLINE, sizeof(char));
     //printf("%p %s\n", line, line);
 
-    while (--maxsize, (len = getline(line, MAXLINE)) > 0) {
+    while (--maxsize, (len = getline(line, MAXLINE, File)) > 0) {
         lineptr[*lineptr_index].str = line;
         lineptr[*lineptr_index].len = len;
         //printf("%d, &d\n", sizeof(line), sizeof(lineptr[lineptr_index++]));
         //printf("%p %s %d\n", line, line, len);
-        printf("%d, %p, %s\n", lineptr[*lineptr_index].len, lineptr[*lineptr_index].str, lineptr[*lineptr_index].str);
+        //printf("%d, %p, %s\n", lineptr[*lineptr_index].len, lineptr[*lineptr_index].str, lineptr[*lineptr_index].str);
 
         ++* lineptr_index;
         line = calloc(MAXLINE, sizeof(char));
