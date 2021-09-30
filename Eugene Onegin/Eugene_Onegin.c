209661091e0fc0fddc1715f2efe64eb8e172df4b
cherrrypok <tashchershit@gmail.com>
2021-09-30 04:42:05 +0300

    Comp

--- Eugene Onegin/Eugene_Onegin.c
+++ Eugene Onegin/Eugene_Onegin.c
@@ -8,10 +8,14 @@ struct String {
     int len;
 };
 
+int getline(char* s, int maxsize, FILE *File);
 int Read_strings(struct String* lineptr, int maxsize, int* lines, FILE *File);
 int Write_strings(struct String* lineptr, int* lines);
-int getline(char* s, int maxsize, FILE *File);
+int Sorting_strings(struct String* lineptr, int left, int right);
+int Comp_strings(struct String a, struct String b);
 int Unit_tests(struct String* lineptr);
+int Unit_tests_Comp(struct String* lineptr);
+
 
 int main() {
     FILE *File = fopen("text_input.txt", "r");
@@ -22,6 +26,7 @@ int main() {
     Write_strings(lineptr, &lines);
     //printf("%d\n", lines);
     //Unit_tests(lineptr);
+    Unit_tests_Comp(lineptr);
 }
 
 int Unit_tests(struct String* lineptr) {
@@ -62,4 +67,32 @@ int Read_strings(struct String* lineptr, int maxsize, int* lineptr_index, FILE *
 int Write_strings(struct String* lineptr, int* lines) {
     for (int i = 0; i < *lines; i++)
         printf("%s", lineptr[i].str);
+}
+
+int Sorting_strings(struct String* lineptr, int left, int right){
+    return 0;
+}
+
+int Comp_strings(struct String struct_a, struct String struct_b){
+    char* a = struct_a.str;
+    char* b = struct_b.str;
+    int i = 0;
+    for (i = 0; a[i] == b[i]; i++){
+        if (a[i] == '\0')
+            return 0;
+    }
+    return a[i] - b[i];
+}
+
+int Unit_tests_Comp(struct String* lineptr){
+    struct String a, b;
+    a.str = "aaba";
+    b.str = "aab";
+    int res = Comp_strings(a, b);
+    if (res < 0)
+        printf("%s < %s\n", a.str, b.str);
+    else if (res == 0)
+        printf("%s = %s\n", a.str, b.str);
+    else
+        printf("%s > %s\n", a.str, b.str);
 }
