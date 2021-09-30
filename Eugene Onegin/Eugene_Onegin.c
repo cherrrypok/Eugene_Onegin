2e6ba218857748b05babc0334c5f7d32825f2b26
cherrrypok <tashchershit@gmail.com>
2021-09-30 19:53:07 +0300

    comp tips

--- Eugene Onegin/Eugene_Onegin.c
+++ Eugene Onegin/Eugene_Onegin.c
@@ -9,24 +9,25 @@ struct String {
 };
 
 int getline(char* s, int maxsize, FILE *File);
+int min(int a, int b);
 int Read_strings(struct String* lineptr, int maxsize, int* lines, FILE *File);
 int Write_strings(struct String* lineptr, int* lines);
 int Sorting_strings(struct String* lineptr, int left, int right);
-int Comp_strings(struct String a, struct String b);
-int Unit_tests(struct String* lineptr);
-int Unit_tests_Comp(struct String* lineptr);
+int Comp_strings(struct String a, struct String b, int tipe); // <0 a < b  >0 a >b
+int Unit_tests(struct String* lineptr); //1 - сортировка с начала 2 - сортировка с конца
+int Unit_tests_Comp(struct String* lineptr, int tipe);
 
 
 int main() {
     FILE *File = fopen("text_input.txt", "r");
-    int lines = 0;
+    int lines = 0, tipe = 1;
     struct String lineptr[MAXLINES];
 
     Read_strings(lineptr, MAXLINES, &lines, File);
     Write_strings(lineptr, &lines);
     //printf("%d\n", lines);
     //Unit_tests(lineptr);
-    Unit_tests_Comp(lineptr);
+    Unit_tests_Comp(lineptr, tipe);
 }
 
 int Unit_tests(struct String* lineptr) {
@@ -45,6 +46,12 @@ int getline(char* s, int maxsize, FILE *File) {
         s[i++] = '\n';
     s[i] = '\0';
     return --i;
+}
+
+int min(int a, int b){
+    if (a < b)
+        return a;
+    return b;
 }
 
 int Read_strings(struct String* lineptr, int maxsize, int* lineptr_index, FILE *File) {
@@ -73,22 +80,36 @@ int Sorting_strings(struct String* lineptr, int left, int right){
     return 0;
 }
 
-int Comp_strings(struct String struct_a, struct String struct_b){
+int Comp_strings(struct String struct_a, struct String struct_b, int tipe){
     char* a = struct_a.str;
     char* b = struct_b.str;
     int i = 0;
-    for (i = 0; a[i] == b[i]; i++){
-        if (a[i] == '\0')
-            return 0;
+    if (tipe == 1) {
+        for (i = 0; a[i] == b[i]; i++){
+            if (a[i] == '\0')
+                return 0;
+        }
+        return a[i] - b[i];
     }
-    return a[i] - b[i];
+
+    if (tipe == 2) {
+        int index = min(struct_a.len, struct_b.len);
+        //printf("%d", index);
+        for (i = index; a[i] == b[i]; i--){
+            if (i == 0)
+                return 0;
+        }
+        return a[i] - b[i];
+    }
 }
 
-int Unit_tests_Comp(struct String* lineptr){
+int Unit_tests_Comp(struct String* lineptr, int tipe){
     struct String a, b;
-    a.str = "aaba";
-    b.str = "aab";
-    int res = Comp_strings(a, b);
+    a.str = "babc";
+    a.len = 4;
+    b.str = "abc";
+    b.len = 3;
+    int res = Comp_strings(a, b, 2);
     if (res < 0)
         printf("%s < %s\n", a.str, b.str);
     else if (res == 0)
